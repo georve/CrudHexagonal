d44af7f9241007dde2cf198e014f55447aafe1c9
Georman <georman.calderon@cleteci.com>
2024-04-24 07:36:58 -0400

    feature: configurations form H2 and loading data

--- src/main/java/com/salesmanagement/salesmagament/infraestructure/adapters/output/persistence/repository/PriceRepository.java
+++ src/main/java/com/salesmanagement/salesmagament/infraestructure/adapters/output/persistence/repository/PriceRepository.java
@@ -4,10 +4,12 @@ import com.salesmanagement.salesmagament.infraestructure.adapters.output.persist
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.time.LocalDateTime;
 
+@Repository
 public interface PriceRepository extends JpaRepository<PriceEntity, Long> {
     
 
